a736f0ebe95636927e6051e5f78970d602a4aab0
itsjustonyii <onyinyechiokeke733@gmail.com>
2023-06-08 22:23:01 +0100

    number of employees, payment id, payment and email redirect all fixed

---
+++ src/components/SuccessModal/Employeeno.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { setAuthToken } from "../setAuthToken";
+import axios from "axios";
+
+function Employeeno() {
+  const [item, setItem] = useState([]);
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://autopay-qv54.onrender.com/api/v1/employee", {
+        headers: {
+          'Authorization': `${setAuthToken()}`,
+        },
+      });
+      setItem(response.data.data);
+    } catch (error) {
+      console.log("Error: ", error)
+    }
+  };
+
+  return (
+    <>
+     {item.length} 
+    </>
+  )
+}
+
+export default Employeeno;
